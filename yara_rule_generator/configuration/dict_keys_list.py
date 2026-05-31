a31d9057eb9858cff9d7329c529b58498ba32c81
Rajaram <rajramdsci@gmail.com>
2026-05-31 23:19:55 +0530

    Added dict key details

---
+++ yara_rule_generator/configuration/dict_keys_list.py
@@ -0,0 +1,25 @@
+
+    # Define key_details as per the query example
+key_details = {
+
+    'key2': {
+        'Detail': 'behavior->summary->files,keys',
+        'mode': 'exclude'
+    },
+    'key3': {
+        'Detail': 'debug',
+        'mode': 'include'
+    },
+    'key4': {
+        'Detail': 'url_analysis',
+        'mode': 'include'
+    },
+    'key5': {
+        'Detail': 'signatures',
+        'mode': 'include'
+    }, 
+    'key6': {
+        'Detail': 'CAPE->payloads->[0..n]->strings',
+        'mode': 'include'
+    }
+}
